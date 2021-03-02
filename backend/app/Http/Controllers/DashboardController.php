6b31f5331a06e000dc30fe527a872d785e45bb0f
alhilaladamu <alhilaladam@gmail.com>
2021-03-02 09:28:28 -0800

    Complete Backend Futures

---
+++ backend/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Complaint;
+use App\Models\Transfer;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+    public function index() {
+        $userId = auth()->user()->id;
+        $matchThis = ['user_id' => $userId, 'type'=> 'AIRTIME'];
+        $airtimeTransfers = Transfer::where($matchThis)->get();
+        $numberOfAirtimeTransfers = count($airtimeTransfers);
+        
+        $matchThis = ['user_id'=> $userId, 'type' => 'DATA'];
+        $dataTransfers = Transfer::where($matchThis)->get();
+        $numberOfDataTransfers = count($dataTransfers);
+
+        $numberOfComplaints = Complaint::where('user_id', $userId)->count();
+        return response()->json(compact('numberOfAirtimeTransfers', 'numberOfDataTransfers',
+        'numberOfComplaints'));
+    }
+
+        public function adminIndex() {
+           
+            $matchThis = ['type'=> 'AIRTIME'];
+            $airtimeTransfers = Transfer::where($matchThis)->get();
+            $numberOfAirtimeTransfers = count($airtimeTransfers);
+
+            $matchThis = ['type' => 'DATA'];
+            $dataTransfers = Transfer::where($matchThis)->count();
+            
+            
+            $unresolvedComplains = Complaint::where('is_resolved', 0)->count();
+            $resolvedComplaints = Complaint::where('is_resolved', 1)->count();
+            $numberOfComplaints = Complaint::all()->count();
+
+            return response()->json(compact('numberOfAirtimeTransfers', 'dataTransfers',
+            'numberOfComplaints', 'unresolvedComplains', 'resolvedComplaints'));
+        }
+}
